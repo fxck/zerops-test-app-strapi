16aef82930d03bf99faeccf1c8afedae42823846
Ales Rechtorik <foxxcz@gmail.com>
2020-08-13 14:08:26 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '322680eecf886203d50928f183bbe6d7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1f551c50-355a-4ed1-b045-087dbc2890d4'
+};
